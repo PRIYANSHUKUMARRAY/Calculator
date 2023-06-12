56a131647c14e2b09be9f0bf2e708d18aa7636be
Priyanshu Kumar Ray <priyanshukray802223@gmail.com>
2023-06-13 01:00:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculator/splace.java
@@ -0,0 +1,31 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+
+public class splace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splace);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent iHome = new Intent(splace.this,MainActivity.class);
+                startActivities(new Intent[]{iHome});
+                finish();
+
+            }
+        },4000);
+
+
+
+
+    }
+
+}
